c9a7619b4a0e60ad36462dfe0a9c805da7947f0f
Brianoyaro <134777190+Brianoyaro@users.noreply.github.com>
2025-10-18 23:52:54 +0300

    Update CORS configuration for API routes
    Restrict CORS to the specified frontend domain for API routes.

--- v2/FlaskBackend/App/__init__.py
+++ v2/FlaskBackend/App/__init__.py
@@ -6,7 +6,11 @@ from .extensions import db, migrate, bcrypt, mail, oauth, jwt
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
-    CORS(app)
+    #CORS(app)
+    # ✅ Restrict CORS to your frontend domain for all API routes
+    CORS(app, resources={
+        r"/api/*": {"origins": ["https://timamu-v2-flaskfrontend.onrender.com"]}
+    })
     
     # Initialize extensions
     db.init_app(app)
