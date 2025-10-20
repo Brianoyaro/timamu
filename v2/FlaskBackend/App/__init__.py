9d92932a0dcf57eb6cb1b5c575be0d3dcfd4f518
Brianoyaro <134777190+Brianoyaro@users.noreply.github.com>
2025-10-20 11:19:32 +0300

    Update CORS configuration in Flask app
    Commented out the original CORS initialization and updated it to specify resources and origins.

--- v2/FlaskBackend/App/__init__.py
+++ v2/FlaskBackend/App/__init__.py
@@ -7,7 +7,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
     
-    CORS(app)
+    #CORS(app)
     CORS(app, resources={r"/api/*": {"origins": "*"}})
     
     # Initialize extensions
